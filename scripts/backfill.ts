3e8312165b297a8bb67ce830388bed010ce15f29
bracesproul <braceasproul@gmail.com>
2025-02-19 14:21:03 -0800

    cr

--- scripts/backfill.ts
+++ scripts/backfill.ts
@@ -50,13 +50,6 @@ export async function backfillWithLinks() {
   });
 
   const newLinksArr: string[] = [
-    "https://twitter.com/387320120/status/1892084239367025151",
-    "https://www.reddit.com/r/LangChain/comments/1iri3zh/looking_for_contributors_expanding_the_brag/",
-    "https://www.youtube.com/watch?v=7GAKB21lxyg&list=PLp01ObP3udmq2quR-RfrX4zNut_t_kNot",
-    "https://www.reddit.com/r/LangChain/comments/1is705t/finetuning_embeddings_for_rag/",
-    "https://x.com/TRJ_0751/status/1891474766621769942",
-    "https://twitter.com/1136237902034296832/status/1891533068227248219",
-    "https://x.com/CopilotKit/status/1891536875648626852",
     // Add your new links here
   ];
 
